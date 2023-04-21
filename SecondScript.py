85011f4706e54d42e517dfc1b799c2488edfaf6d
ICEWOLFex <isip_d.a.podlesniy@mpt.ru>
2023-04-21 15:40:36 +0300

    new

--- SecondScript.py
+++ SecondScript.py
@@ -11,14 +11,4 @@ fulYearDays = 0
 for month in range(12):
     for days in range(year[month] + 1):
         fulYearDays += days//10+days%10
-# oneDays = (1*13+2*12+3*5+4*3+5*3+6*3+7*3+8*3+9*3)*7
-# zeroDays =(1*12+2*12+3*4+4*3+5*3+6*3+7*3+8*3+9*3)*4
-# if feb == 29:
-#    febDays = 1*12+2*12+3*3+4*3+5*3+6*3+7*3+8*3+9*3
-# elif feb == 28:
-#     febDays = 1*12+2*11+3*3+4*3+5*3+6*3+7*3+8*3+9*2
-# else:
-#     print("Error")
-
-# fulYearDays = oneDays+zeroDays+febDays
 print(fulYearDays)
